feat: Improve normalisation to not have pair-wise, channel-wise normalization Fixes #82

models/cond_vae.py
@@ -7,7 +7,6 @@ from torch.nn import functional as F
 from tqdm import tqdm
 
 from loss import cond_loss
-from utils import get_contrast
 
 from .base import BaseVAE
 from .layers import down_block, up_block
@@ -521,14 +520,14 @@ class Cond_SRVAE(BaseVAE):
 
                 if first:
                     imgs = {
-                        "y": y[:4],
-                        "x": x[:4],
+                        "y": y[:4, [2, 1, 0], :, :],
+                        "x": x[:4, [2, 1, 0], :, :],
                         "y_bicubic": F.interpolate(y, scale_factor=2, mode="bicubic")[
-                            :4
+                            :4, [2, 1, 0], :, :
                         ],  # Bicubic interpolation for y
-                        "y_hat": y_hat[:4],
-                        "x_hat": x_hat[:4],
-                        "x_sr": x_sr[:4],
+                        "y_hat": y_hat[:4, [2, 1, 0], :, :],
+                        "x_hat": x_hat[:4, [2, 1, 0], :, :],
+                        "x_sr": x_sr[:4, [2, 1, 0], :, :],
                     }
                     first = False
 
@@ -556,18 +555,15 @@ class Cond_SRVAE(BaseVAE):
                 x_hat, y_hat, *_ = self.forward(x, y)
                 x_sr = self.conditional_generation(y)
 
-            gt_min, gt_max = get_contrast(x)
-
             imgs = {
-                "y": ((y[:4] - gt_min) / (gt_max - gt_min)),  # Normalize y
-                "x": ((x[:4] - gt_min) / (gt_max - gt_min)),
-                "y_bicubic": (
-                    (F.interpolate(y, scale_factor=2, mode="bicubic")[:4] - gt_min)
-                    / (gt_max - gt_min)
-                ),  # Bicubic interpolation for y
-                "y_hat": ((y_hat[:4] - gt_min) / (gt_max - gt_min)),
-                "x_hat": ((x_hat[:4] - gt_min) / (gt_max - gt_min)),
-                "x_sr": ((x_sr[:4] - gt_min) / (gt_max - gt_min)),
+                "y": y[:4, [2, 1, 0], :, :],  # RGB channels
+                "x": x[:4, [2, 1, 0], :, :],
+                "y_bicubic": F.interpolate(y, scale_factor=2, mode="bicubic")[
+                    :4, [2, 1, 0], :, :
+                ],  # Bicubic interpolation for y
+                "y_hat": y_hat[:4, [2, 1, 0], :, :],
+                "x_hat": x_hat[:4, [2, 1, 0], :, :],
+                "x_sr": x_sr[:4, [2, 1, 0], :, :],
             }
 
         if epoch % 10 == 0 or epoch == 1:

models/vae.py
@@ -3,7 +3,6 @@ import torch.nn as nn
 import wandb
 
 from loss import base_loss
-from utils import get_contrast
 
 from .base import BaseVAE
 from .layers import down_block, up_block
@@ -193,11 +192,10 @@ class VAE(BaseVAE):
             x, _ = batch
             x = x.to(device)
 
-            gt_min, gt_max = get_contrast(x)
             with torch.no_grad():
                 x_hat, _, _ = self.forward(x)
-                imgs_in = (x[:4] - gt_min) / (gt_max - gt_min)
-                imgs_out = (x_hat[:4] - gt_min) / (gt_max - gt_min)
+                imgs_in = x[:4]
+                imgs_out = x_hat[:4]
 
         # log sample images
         if epoch % 5 == 0 or epoch == 1: