30eb53bf1163f868e9acf6735a07a0c88fbd25cc
Etienne <etienne.bardet@gmail.com>
2025-06-20 17:07:27 +0200

    expe: revert double u,y enc paths

--- models/cond_vae.py
+++ models/cond_vae.py
@@ -30,15 +30,15 @@ class Cond_SRVAE(BaseVAE):
             nn.Conv2d(
                 in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1
             ),
-            nn.ReLU(),
+            nn.LeakyReLU(),
             nn.Conv2d(
                 in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1
             ),
-            nn.ReLU(),
+            nn.LeakyReLU(),
             nn.Conv2d(
                 in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1
             ),
-            nn.ReLU(),
+            nn.LeakyReLU(),
             nn.Conv2d(
                 in_channels=128,
                 out_channels=(self.latent_size_y // 64)
@@ -71,32 +71,23 @@ class Cond_SRVAE(BaseVAE):
             nn.Conv2d(
                 in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1
             ),
-            nn.ReLU(),
+            nn.LeakyReLU(),
             nn.Conv2d(
                 in_channels=64, out_channels=16, kernel_size=3, stride=1, padding=1
             ),
-            nn.ReLU(),
+            nn.LeakyReLU(),
             nn.Conv2d(
                 in_channels=16, out_channels=16, kernel_size=3, stride=1, padding=1
             ),
-            nn.ReLU(),
+            nn.LeakyReLU(),
             nn.Conv2d(
                 in_channels=16, out_channels=4, kernel_size=3, stride=1, padding=1
             ),
             nn.Sigmoid(),  # Ensure output is in [0, 1]
         )
 
-        self.pre_enc_x = down_block(in_channels=4, out_channels=4)
-
         self.encoder_x = nn.Sequential(
-            nn.Conv2d(
-                in_channels=8, out_channels=8, kernel_size=3, stride=1, padding=1
-            ),
-            nn.ReLU(),
-            nn.Conv2d(
-                in_channels=8, out_channels=16, kernel_size=3, stride=1, padding=1
-            ),
-            nn.ReLU(),
+            down_block(in_channels=4, out_channels=16),  # out 16, 32, 32
             down_block(in_channels=16, out_channels=64),
             down_block(
                 in_channels=64,
@@ -105,15 +96,15 @@ class Cond_SRVAE(BaseVAE):
             nn.Conv2d(
                 in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1
             ),
-            nn.ReLU(),
+            nn.LeakyReLU(),
             nn.Conv2d(
                 in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1
             ),
-            nn.ReLU(),
+            nn.LeakyReLU(),
             nn.Conv2d(
                 in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1
             ),
-            nn.ReLU(),
+            nn.LeakyReLU(),
             nn.Conv2d(
                 in_channels=128,
                 out_channels=(self.latent_size // 64),
@@ -121,39 +112,39 @@ class Cond_SRVAE(BaseVAE):
                 stride=1,
                 padding=1,
             ),
-            nn.ReLU(),
+            nn.LeakyReLU(),
             # out 1024 * 4 * 4 = 16384
         )  # out 1024, 4, 4
 
         self.z_carac = nn.Sequential(
             down_block(
-                in_channels=self.latent_size * 2 // 64,
-                out_channels=self.latent_size * 2 // 32,
+                in_channels=self.latent_size * 3 // 64,
+                out_channels=self.latent_size * 3 // 32,
             ),
             nn.Conv2d(
-                in_channels=self.latent_size * 2 // 32,
-                out_channels=self.latent_size * 2 // 32,
+                in_channels=self.latent_size * 3 // 32,
+                out_channels=self.latent_size * 3 // 32,
                 kernel_size=3,
                 stride=1,
                 padding=1,
             ),
-            nn.ReLU(),
+            nn.LeakyReLU(),
             nn.Conv2d(
-                in_channels=self.latent_size * 2 // 32,
+                in_channels=self.latent_size * 3 // 32,
+                out_channels=self.latent_size * 3 // 16,
+                kernel_size=3,
+                stride=1,
+                padding=1,
+            ),
+            nn.LeakyReLU(),
+            nn.Conv2d(
+                in_channels=self.latent_size * 3 // 16,
                 out_channels=self.latent_size * 2 // 16,
                 kernel_size=3,
                 stride=1,
                 padding=1,
             ),
-            nn.ReLU(),
-            nn.Conv2d(
-                in_channels=self.latent_size * 2 // 16,
-                out_channels=self.latent_size * 2 // 16,
-                kernel_size=3,
-                stride=1,
-                padding=1,
-            ),
-            nn.ReLU(),
+            nn.LeakyReLU(),
             nn.Conv2d(
                 in_channels=self.latent_size * 2 // 16,
                 out_channels=self.latent_size * 2 // 16,
@@ -188,15 +179,15 @@ class Cond_SRVAE(BaseVAE):
             nn.Conv2d(
                 in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1
             ),
-            nn.ReLU(),
+            nn.LeakyReLU(),
             nn.Conv2d(
                 in_channels=64, out_channels=16, kernel_size=3, stride=1, padding=1
             ),
-            nn.ReLU(),
+            nn.LeakyReLU(),
             nn.Conv2d(
                 in_channels=16, out_channels=16, kernel_size=3, stride=1, padding=1
             ),
-            nn.ReLU(),
+            nn.LeakyReLU(),
             nn.Conv2d(
                 in_channels=16, out_channels=4, kernel_size=3, stride=1, padding=1
             ),
@@ -209,15 +200,15 @@ class Cond_SRVAE(BaseVAE):
             nn.Conv2d(
                 in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1
             ),
-            nn.ReLU(),
+            nn.LeakyReLU(),
             nn.Conv2d(
                 in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1
             ),
-            nn.ReLU(),
+            nn.LeakyReLU(),
             nn.Conv2d(
                 in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1
             ),
-            nn.ReLU(),
+            nn.LeakyReLU(),
             nn.Conv2d(
                 in_channels=128,
                 out_channels=self.latent_size // 64,
@@ -244,59 +235,21 @@ class Cond_SRVAE(BaseVAE):
                 kernel_size=3,
                 padding=1,
             ),
-            nn.ReLU(),
+            nn.LeakyReLU(),
             nn.Conv2d(
                 self.latent_size_y // 64,
                 self.latent_size_y // 32,
                 kernel_size=3,
                 padding=1,
             ),
-            nn.ReLU(),
+            nn.LeakyReLU(),
             nn.Conv2d(
                 self.latent_size_y // 32,
                 self.latent_size_y // 32,
                 kernel_size=3,
                 padding=1,
             ),
-            nn.ReLU(),
-            nn.Conv2d(
-                self.latent_size_y // 32,
-                self.latent_size_y // 16,
-                kernel_size=3,
-                padding=1,
-            ),
-        )
-
-        self.u_to_xz = nn.Sequential(
-            nn.Unflatten(
-                1,
-                (
-                    self.latent_size_y // 64,
-                    self.patch_size // 2**3,
-                    self.patch_size // 2**3,
-                ),
-            ),
-            nn.Conv2d(
-                self.latent_size_y // 64,
-                self.latent_size_y // 64,
-                kernel_size=3,
-                padding=1,
-            ),
-            nn.ReLU(),
-            nn.Conv2d(
-                self.latent_size_y // 64,
-                self.latent_size_y // 32,
-                kernel_size=3,
-                padding=1,
-            ),
-            nn.ReLU(),
-            nn.Conv2d(
-                self.latent_size_y // 32,
-                self.latent_size_y // 32,
-                kernel_size=3,
-                padding=1,
-            ),
-            nn.ReLU(),
+            nn.LeakyReLU(),
             nn.Conv2d(
                 self.latent_size_y // 32,
                 self.latent_size_y // 16,
@@ -320,11 +273,11 @@ class Cond_SRVAE(BaseVAE):
                 kernel_size=3,
                 padding=1,
             ),
-            nn.ReLU(),
+            nn.LeakyReLU(),
             nn.Conv2d(
                 self.latent_size // 16, self.latent_size // 16, kernel_size=3, padding=1
             ),
-            nn.ReLU(),
+            nn.LeakyReLU(),
             nn.Conv2d(
                 self.latent_size // 16,
                 self.latent_size * 2 // 16,
@@ -352,11 +305,9 @@ class Cond_SRVAE(BaseVAE):
         y = self.encoder_y(y)
         return torch.chunk(y, 2, dim=1)
 
-    def encode_x(self, x, y):
+    def encode_x(self, x):
         # Define the encoder part of the VAE
-        x = self.pre_enc_x(x)
-        stack = torch.cat((x, y), dim=1)
-        return self.encoder_x(stack)
+        return self.encoder_x(x)
 
     def reparameterize(self, mu, logvar):
         # Reparameterization trick
@@ -374,16 +325,15 @@ class Cond_SRVAE(BaseVAE):
     def forward(self, x, y):
         mu_u, logvar_u = self.encode_y(y)
         u = self.reparameterize(mu_u, logvar_u)
-        x_enc = self.encode_x(x, y)
+        x_enc = self.encode_x(x)
         y_enc = self.y_to_z(y)
-        u_enc = self.u_to_xz(u)
+        u_enc = self.u_to_z(u)
 
         mu_z, logvar_z = torch.chunk(
             self.z_carac(torch.cat((x_enc, u_enc), dim=1)), 2, dim=1
         )
         z = self.reparameterize(mu_z, logvar_z)
 
-        u_enc = self.u_to_z(u)
         y_enc = y_enc.view(y_enc.size(0), -1)
         u_enc = u_enc.view(u_enc.size(0), -1)
         mu_z_uy, logvar_z_uy = self.z_cond(y_enc, u_enc)
--- models/layers.py
+++ models/layers.py
@@ -234,6 +234,7 @@ class down_block(nn.Module):
         self.downsample = nn.Conv2d(
             in_channels, out_channels, kernel_size=4, stride=2, padding=1
         )
+        self.bn_in = nn.BatchNorm2d(in_channels)
         self.bn = nn.BatchNorm2d(out_channels)
         self.relu = nn.ReLU()
 
@@ -247,7 +248,13 @@ class down_block(nn.Module):
         Returns:
             torch.Tensor: Output tensor of shape (batch_size, out_channels, new_height, new_width).
         """
+        inp = x
         x = self.conv(x)
+        if self.with_bn:
+            x = self.bn_in(x)
+        if self.with_relu:
+            x = self.relu(x)
+        x = x + inp  # Residual connection
         x = self.downsample(x)
         if self.with_bn:
             x = self.bn(x)
@@ -275,6 +282,7 @@ class up_block(nn.Module):
         self.upsample = nn.ConvTranspose2d(
             in_channels, out_channels, kernel_size=4, stride=2, padding=1
         )
+        self.bn_in = nn.BatchNorm2d(in_channels)
         self.bn = nn.BatchNorm2d(out_channels)
         self.relu = nn.ReLU()
 
@@ -288,7 +296,13 @@ class up_block(nn.Module):
         Returns:
             torch.Tensor: Output tensor of shape (batch_size, out_channels, new_height, new_width).
         """
+        inp = x
         x = self.conv(x)
+        if self.with_bn:
+            x = self.bn_in(x)
+        if self.with_relu:
+            x = self.relu(x)
+        x = x + inp  # Residual connection
         x = self.upsample(x)
         if self.with_bn:
             x = self.bn(x)
